014fbe98c811f5456f6f4987a0d8b12d581d4cf4
SarahIrons <sarahirons11@gmail.com>
2024-02-20 13:35:59 -0500

    font changes and added list to content section 4

--- src/App.js
+++ src/App.js
@@ -6,10 +6,17 @@ function App() {
   return (
   <>
     <NavigationPanel/>
-    <ContentSection id="about"><h1>Hi, I'm Sarah.</h1></ContentSection>
+    <ContentSection id="about"><h1>Hi, my name is Sarah.</h1></ContentSection>
     <ContentSection id="work" className="two"><h1>I'm a web developer.</h1></ContentSection>
-    <ContentSection id="skills" className="three"><h1>Here is some of my work.</h1></ContentSection>
-    <ContentSection id="contact" className="four"><h1>Reach out.</h1></ContentSection>
+    <ContentSection id="skills" className="three"><h1>My training included: .</h1></ContentSection>
+    <ContentSection id="contact" className="four">
+      <h1>Here is how to reach me:</h1>
+      <ul>
+        <li><a href = "mailto: sarahirons11@gmail.com">sarahirons11@gmail.com</a></li>
+        <li><a href="https://www.linkedin.com/in/sarah-e-irons/">LinkedIn</a></li>
+        <li><a href="https://github.com/SarahIrons">Github</a></li>
+      </ul>
+   </ContentSection>
   </>
   );
 }
