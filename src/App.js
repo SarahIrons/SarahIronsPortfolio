2608f086b1a4b692c2d4b932ae67eb5125196ef0
SarahIrons <sarahirons11@gmail.com>
2024-02-20 14:39:36 -0500

    updated buttons

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.scss';
 import ContentSection from './components/ContentSection';
 import NavigationPanel from './components/Navigation';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin,faSquareGithub} from  "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
@@ -8,13 +10,13 @@ function App() {
     <NavigationPanel/>
     <ContentSection id="about"><h1>Hi, my name is Sarah.</h1></ContentSection>
     <ContentSection id="work" className="two"><h1>I'm a web developer.</h1></ContentSection>
-    <ContentSection id="skills" className="three"><h1>My training included: .</h1></ContentSection>
+    <ContentSection id="skills" className="three"><h1>My training included: ***</h1></ContentSection>
     <ContentSection id="contact" className="four">
       <h1>Here is how to reach me:</h1>
       <ul>
         <li><a href = "mailto: sarahirons11@gmail.com">sarahirons11@gmail.com</a></li>
-        <li><a href="https://www.linkedin.com/in/sarah-e-irons/">LinkedIn</a></li>
-        <li><a href="https://github.com/SarahIrons">Github</a></li>
+        <li><a href="https://www.linkedin.com/in/sarah-e-irons/"><FontAwesomeIcon icon={faLinkedin}/></a></li>
+        <li><a href="https://github.com/SarahIrons"><FontAwesomeIcon icon={faSquareGithub}/></a></li>
       </ul>
    </ContentSection>
   </>
